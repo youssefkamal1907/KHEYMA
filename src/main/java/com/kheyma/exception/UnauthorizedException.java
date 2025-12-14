9a378cdb90058e9b182ac09278a36ef30d61eb2d
Youssef Kamal <65743371+youssefkamal1907@users.noreply.github.com>
2025-12-14 23:38:39 +0200

    Delete src/main directory

--- src/main/java/com/kheyma/exception/UnauthorizedException.java
+++
@@ -1,8 +0,0 @@
-package com.kheyma.exception;
-
-public class UnauthorizedException extends RuntimeException {
-    public UnauthorizedException(String message) {
-        super(message);
-    }
-}
-
